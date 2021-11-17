ba42e078e7a1183da8d222eb601c34fa189bf3a8
dpobera <oberadaveperol@gmail.com>
2021-11-17 15:36:46 +0800

    idssss

--- main/addrecord/orderid/auto-order-id-ep.php
+++ main/addrecord/orderid/auto-order-id-ep.php
@@ -6,7 +6,7 @@ $result = mysqli_query($db, $query);
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
         $newOrderId = $row['ep_id'] + 1;
-        echo "<input style='border:none;background-color:transparent; font-weight:bolder; color:grey;' name='ep_id' value='" . $newOrderId . "'>";
+        echo "<input style='border:none;background-color:transparent; font-weight:bolder; color:grey;' name='ep_id' value='" . str_pad($newOrderId, 8, 0, STR_PAD_LEFT) . "'>";
     }
 } else {
     echo "No result.";
--- main/addrecord/orderid/auto-order-id-jo.php
+++ main/addrecord/orderid/auto-order-id-jo.php
@@ -6,7 +6,7 @@ $result = mysqli_query($db, $query);
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
         $newOrderId = $row['jo_id'] + 1;
-        echo "<input style='border:none;background-color:transparent; font-weight:bolder; color:grey;' name='jo_id' value='" . $newOrderId . "' readonly>";
+        echo "<input style='border:none;background-color:transparent; font-weight:bolder; color:grey;' name='jo_id' value='" . str_pad($newOrderId, 8, 0, STR_PAD_LEFT) . "' readonly>";
     }
 } else {
     echo "No result.";
