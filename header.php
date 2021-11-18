988144f90fccda9efd3cd92018466a9626e0f8c2
dpobera <oberadaveperol@gmail.com>
2021-11-18 10:12:58 +0800

    Update header.php

--- header.php
+++ header.php
@@ -17,107 +17,10 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/js/standalone/selectize.min.js" integrity="sha256-+C0A5Ilqmu4QcSPxrlGpaZxJ04VjsRjKu+G82kl5UJk=" crossorigin="anonymous"></script>
   <title>PACC IMS</title>
-  <!-- <style>
-    /* The Modal (background) */
-    .modal {
-      display: none;
-      /* Hidden by default */
-      position: fixed;
-      /* Stay in place */
-      z-index: 1;
-      /* Sit on top */
-      padding-top: 100px;
-      /* Location of the box */
-      left: 0;
-      top: 0;
-      width: 100%;
-      /* Full width */
-      height: 100%;
-      /* Full height */
-      overflow: auto;
-      /* Enable scroll if needed */
-      background-color: rgb(0, 0, 0);
-      /* Fallback color */
-      background-color: rgba(0, 0, 0, 0.4);
-      /* Black w/ opacity */
-    }
 
-    /* Modal Content */
-    .modal-content {
-      position: relative;
-      background-color: #fefefe;
-      margin: auto;
-      padding: 0;
-      border: 1px solid #888;
-      width: 80%;
-      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-      -webkit-animation-name: animatetop;
-      -webkit-animation-duration: 0.4s;
-      animation-name: animatetop;
-      animation-duration: 0.4s
-    }
-
-    /* Add Animation */
-    @-webkit-keyframes animatetop {
-      from {
-        top: -300px;
-        opacity: 0
-      }
-
-      to {
-        top: 0;
-        opacity: 1
-      }
-    }
-
-    @keyframes animatetop {
-      from {
-        top: -300px;
-        opacity: 0
-      }
-
-      to {
-        top: 0;
-        opacity: 1
-      }
-    }
-
-    /* The Close Button */
-    .close {
-      color: white;
-      float: right;
-      font-size: 28px;
-      font-weight: bold;
-    }
-
-    .close:hover,
-    .close:focus {
-      color: #000;
-      text-decoration: none;
-      cursor: pointer;
-    }
-
-    .modal-header {
-      padding: 2px 16px;
-      background-color: #5cb85c;
-      color: white;
-    }
-
-    .modal-body {
-      padding: 2px 16px;
-    }
-
-    .modal-footer {
-      padding: 2px 16px;
-      background-color: #5cb85c;
-      color: white;
-    }
-  </style> -->
 </head>
 
 <body style="margin: 0px;" bgcolor="#B0C4DE">
-
-
   <div class="navbar" <?php
                       if (!isset($_SESSION['user'])) {
                         echo "hidden";
@@ -174,7 +77,7 @@
     <a href="php/logout-inc.php" style="float:right;" title="Sign-Out">Welcome &nbsp; <i class='fas fa-user-circle'></i>&nbsp;<?php echo $_SESSION["empName"]; ?></a>
   </div>
 
-
+  <!-- 
   <script type='text/javascript'>
     function showSrr() {
       //set the width and height of the 
@@ -202,4 +105,4 @@
         "menubar=no,resizable=yes,width=1600,height=1000,scrollbars=yes,left=" +
         left + ",top=" + top + ",screenX=" + left + ",screenY=" + top);
     }
-  </script>
+  </script> -->
