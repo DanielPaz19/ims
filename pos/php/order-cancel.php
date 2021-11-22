faf0921eb3582d14262d0661101b29b2a488d0d1
DanielPaz19 <88175231+DanielPaz19@users.noreply.github.com>
2021-11-22 19:12:03 +0800

    Update order-cancel.php

--- pos/php/order-cancel.php
+++ pos/php/order-cancel.php
@@ -7,17 +7,60 @@ $userLevel = $_SESSION['level'];
 //     exit();
 // }
 
-$orderId = $_POST['orderId'];
+$orderId = number_format($_POST['orderId']);
 require 'config.php';
 
 
+// Change status of order on order_tb
 if (!mysqli_query($db, "UPDATE order_tb SET order_status_id = '4' WHERE order_id = '$orderId'")) {
-  echo "Error on SQL";
+  echo "Error Order Table Update";
   exit();
 }
 
-echo "Order Successfully Cancelled!";
 
-// Change status of order on order_tb
 // Change qty of products in product_tb based on order_product
-// Update in_qty on Item movement
+//  - SELECT product_id, qty from order_product where order_id = $orderId
+$result =  mysqli_query(
+  $db,
+  "SELECT order_product.product_id, order_product.pos_temp_qty, product.qty
+  FROM order_product 
+  LEFT JOIN product ON order_product.product_id = product.product_id
+  WHERE order_product.order_id = $orderId"
+);
+
+if (!$result) {
+  echo "Error Item Fetch";
+  exit();
+}
+
+//  - For each product add the qty to product_tb 
+if (mysqli_num_rows($result) > 0) {
+  while ($row = mysqli_fetch_assoc($result)) {
+    $productId[] = $row['product_id'];
+    $orderQty[] = $row['pos_temp_qty'];
+    $productQty[] = $row['qty'];
+  }
+}
+
+foreach ($productId as $index => $id) {
+  $newQty = $orderQty[$index] + $productQty[$index];
+  $balQty = $productQty[$index];
+  $inQty = $orderQty[$index];
+
+  if (!mysqli_query($db, "UPDATE product SET qty='$newQty' WHERE product_id = '$id'")) {
+    echo "Error updating product table.";
+    exit();
+  };
+
+  if (!mysqli_query(
+    $db,
+    "INSERT INTO move_product(product_id, bal_qty, in_qty, mov_type_id, move_ref)
+    VALUES('$id', '$balQty', '$inQty', '6', '$orderId')"
+  )) {
+    echo "Error updating movement table.";
+    exit();
+  };
+}
+
+
+echo "Order Successfully Cancelled!";
