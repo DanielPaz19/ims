e1a8863f4af48ac266c4432a0c19941457be8a06
dpobera <oberadaveperol@gmail.com>
2021-11-27 14:02:03 +0800

    product_type

--- itemlist_main.php
+++ itemlist_main.php
@@ -340,25 +340,25 @@ if (!isset($_SESSION['user'])) {
                     <table width="100%">
                         <tr>
                             <th style="text-align: left;" width="30%">Item Description&nbsp;<i style="color: red;" class='fas fa-exclamation-circle'></i></th>
-                            <th style="text-align: left;" width="30%">Supplier</th>
+                            <th style="text-align: center;" width="30%">Standard Item</th>
                             <th style="text-align: left;" width="40%"></th>
 
                         </tr>
                         <tr>
                             <td> <input type="text" name="product_name" style=" width:460px;border: 1px solid gray; height: 36px; border-radius: 5px;" required tabindex="1"></td>
 
-                            <td colspan="4" class='input__container'><input type="hidden" name="sup_id" class="container--supplier__id"><input name="sup_name" style="width:500px; height: 35px; border: 1px solid gray; border-radius: 5px;" class="input__search--supplier modal__trigger--supplier" tabindex="-1">
+                            <!-- <td colspan="4" class='input__container'><input type="hidden" name="sup_id" class="container--supplier__id"><input name="sup_name" style="width:500px; height: 35px; border: 1px solid gray; border-radius: 5px;" class="input__search--supplier modal__trigger--supplier" tabindex="-1">
                                 <div class="list__container--supplier list__container">
                                     <ul class="list list--supplier">
 
                                     </ul>
 
                                 </div>
+                            </td> -->
+                            <td style="text-align: center;"> <input style=" top: 0;left: 0;height: 25px;width: 25px;background-color: midnightblue;" type="checkbox" name="product_type" value="1" checked="checked"><br>&nbsp; <i style="color: chocolate;">(uncheck if non-inventory type item)</i>
                             </td>
-                            <td></td>
                         </tr>
                     </table>
-
                     <br>
                     <table width="100%">
                         <tr>
