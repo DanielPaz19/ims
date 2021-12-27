467f312a56d68ca457ee4c5085235fb53d308044
DanielPaz19 <anyepaz1019@gmail.com>
2021-12-27 09:59:04 +0800

    jo-api

--- pos/php/jo.php
+++ pos/php/jo.php
@@ -11,8 +11,16 @@ if (mysqli_num_rows($resultJo) > 0) {
   while ($rowJo = mysqli_fetch_assoc($resultJo)) {
 
     // Run Select for product array for each jo_id
+    $qryJoItems = "SELECT jo_product.product_id, jo_product.jo_id, jo_product.jo_product_qty, jo_product.jo_product_price,
+    jo_product.jo_remarks FROM jo_product WHERE jo_product.jo_id =" . $rowJo['jo_id'];
+    $resultJoItems = mysqli_query($db, $qryJoItems);
 
-    $rowJo['items'] = [];
+    if (mysqli_num_rows($resultJoItems) > 0) {
+      while ($rowJoItems = mysqli_fetch_assoc($resultJoItems)) {
+        $rowJo['items'][] = $rowJoItems;
+      }
+    }
+
     $output[] = $rowJo;
     // Insert product Id on product Array based on jo_id and push to each output
 
