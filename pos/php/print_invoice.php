29d1c191291ea1f9d349738ae3a8d4a69e019fe0
DanielPaz19 <anyepaz1019@gmail.com>
2022-01-17 22:25:29 +0800

    Invoice Printing p1

---
+++ pos/php/print_invoice.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/w3.css">
+    <link rel="stylesheet" href="../styles/w3-customs.css">
+    <link rel="stylesheet" href="../styles/receipt-print.css">
+    <script defer src="../js/receipt-print.js"></script>
+    <title>Sales Invoice</title>
+</head>
+
+<body class="w3-light-gray">
+    <div class="w3-white" style="width: 8.5in; height: 11in;">
+        <div class="w3-cell-row  w3-red w3-leftbar w3-rightbar" style="height:100%;">
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
