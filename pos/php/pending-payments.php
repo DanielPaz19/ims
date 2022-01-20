a2c35600042c6f11f1ea43836a008853d51ac28e
DanielPaz19 <anyepaz1019@gmail.com>
2022-01-20 10:16:05 +0800

    bug fix

--- pos/php/pending-payments.php
+++ pos/php/pending-payments.php
@@ -1,7 +1,7 @@
 <?php
 include "config.php";
 
-// $joId = $_POST['joId'];
+$joId = $_POST['joId'];
 $output = [];
 
 $query = "SELECT order_tb.jo_id, jo_tb.jo_no, order_tb.order_status_id, order_tb.order_id, order_tb.customer_id, order_tb.total, order_tb.pos_date, 
@@ -10,7 +10,7 @@ $query = "SELECT order_tb.jo_id, jo_tb.jo_no, order_tb.order_status_id, order_tb
   LEFT JOIN jo_tb ON jo_tb.jo_id = order_tb.jo_id
   LEFT JOIN customers ON customers.customers_id = order_tb.customer_id
   LEFT JOIN order_payment ON order_tb.order_id = order_payment.order_id
-  WHERE order_tb.jo_id = '95' ORDER BY order_tb.order_id DESC";
+  WHERE order_tb.jo_id = '$joId' ORDER BY order_tb.order_id DESC";
 
 $result = mysqli_query($db, $query);
 
