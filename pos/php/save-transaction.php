297338f5d0fe6730ffeb968dd251b352cc102ade
DanielPaz19 <anyepaz1019@gmail.com>
2022-01-29 17:03:05 +0800

    re-merge

--- pos/php/save-transaction.php
+++ pos/php/save-transaction.php
@@ -1,66 +1,69 @@
 <?php
-session_start();
-date_default_timezone_set("Asia/Manila");
-include 'config.php';
 
-$transDetails = json_decode($_POST['json']);
-$customerId = $transDetails->customerId;
-$productId = $transDetails->productId;
-$productQty = $transDetails->qty;
-$productPrice = $transDetails->price;
-$discount = $transDetails->discount;
-$orderId = $transDetails->transactionId;
-$total = $transDetails->total;
-$joId = $transDetails->joId;
-$userId = $_SESSION['id'];
+if ($_POST['json']) {
+  session_start();
+  date_default_timezone_set("Asia/Manila");
+  include 'config.php';
 
-$lastQty = [];
+  $transDetails = json_decode($_POST['json']);
+  $customerId = $transDetails->customerId;
+  $productId = $transDetails->productId;
+  $productQty = $transDetails->qty;
+  $productPrice = $transDetails->price;
+  $discount = $transDetails->discount;
+  $orderId = $transDetails->transactionId;
+  $total = $transDetails->total;
+  $joId = $transDetails->joId;
+  $userId = $_SESSION['id'];
 
-$query = "INSERT INTO order_tb (customer_id, total, order_status_id, user_id, jo_id) 
-  VALUES ('$customerId','$total','1', '$userId', '$joId');";
+  $lastQty = [];
 
-$query2 = "INSERT INTO order_payment (order_id, payment_type_id, order_payment_credit, order_payment_balance,  payment_status_id)
-  VALUES ('$orderId','0','$total','$total', '1');";
+  $query = "INSERT INTO order_tb (customer_id, total, order_status_id, user_id, jo_id) 
+    VALUES ('$customerId','$total','1', '$userId', '$joId');";
 
-mysqli_query($db, $query);
-mysqli_query($db, $query2);
+  $query2 = "INSERT INTO order_payment (order_id, payment_type_id, order_payment_credit, order_payment_balance,  payment_status_id)
+    VALUES ('$orderId','0','$total','$total', '1');";
 
-// Close the jo_tb
-mysqli_query($db, "UPDATE jo_tb SET closed ='1' WHERE jo_id ='$joId'");
+  mysqli_query($db, $query);
+  mysqli_query($db, $query2);
 
-$limit = 0;
-while (sizeof($productId) != $limit) {
+  // Close the jo_tb
+  mysqli_query($db, "UPDATE jo_tb SET closed ='1' WHERE jo_id ='$joId'");
 
-  // Insert order details on order_product
-  $query3 = "INSERT INTO order_product (product_id, order_id, pos_temp_qty, pos_temp_price, pos_temp_disamount) 
-  VALUES ('" . $productId[$limit] . "','" . $orderId . "','" . $productQty[$limit] . "','" . $productPrice[$limit] . "','" . $discount[$limit] . "');";
+  $limit = 0;
+  while (sizeof($productId) != $limit) {
 
-  mysqli_query($db, $query3);
+    // Insert order details on order_product
+    $query3 = "INSERT INTO order_product (product_id, order_id, pos_temp_qty, pos_temp_price, pos_temp_disamount) 
+    VALUES ('" . $productId[$limit] . "','" . $orderId . "','" . $productQty[$limit] . "','" . $productPrice[$limit] . "','" . $discount[$limit] . "');";
 
-  // Subract qty ordered from product table
-  $query4 = "SELECT qty FROM product WHERE product_id=" . $productId[$limit];
-  $result = mysqli_query($db, $query4);
+    mysqli_query($db, $query3);
 
-  // Run Loop for each product 
-  if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-      $currentQty = $row['qty'];
+    // Subract qty ordered from product table
+    $query4 = "SELECT qty FROM product WHERE product_id=" . $productId[$limit];
+    $result = mysqli_query($db, $query4);
 
-      // Update current product qty
-      $sqlUpdateQty = "UPDATE product SET qty=" . ($currentQty - $productQty[$limit]) . " WHERE product_id =" . $productId[$limit];
+    // Run Loop for each product 
+    if (mysqli_num_rows($result) > 0) {
+      while ($row = mysqli_fetch_assoc($result)) {
+        $currentQty = $row['qty'];
 
-      mysqli_query($db, $sqlUpdateQty);
+        // Update current product qty
+        $sqlUpdateQty = "UPDATE product SET qty=" . ($currentQty - $productQty[$limit]) . " WHERE product_id =" . $productId[$limit];
 
-      // Record item movement
-      $sqlItemMov = "INSERT INTO move_product(product_id, bal_qty, out_qty, mov_type_id, move_ref)
-      VALUES('" . $productId[$limit] . "','" . $currentQty . "','" . $productQty[$limit] . "','4','" . $orderId . "')";
+        mysqli_query($db, $sqlUpdateQty);
 
-      mysqli_query($db, $sqlItemMov);
-    };
+        // Record item movement
+        $sqlItemMov = "INSERT INTO move_product(product_id, bal_qty, out_qty, mov_type_id, move_ref)
+        VALUES('" . $productId[$limit] . "','" . $currentQty . "','" . $productQty[$limit] . "','4','" . $orderId . "')";
+
+        mysqli_query($db, $sqlItemMov);
+      };
+    }
+
+
+    $limit++;
   }
 
-
-  $limit++;
+  // echo $query;
 }
-
-// echo $query;
