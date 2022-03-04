de2f5a4d072e7bff8e2c4c299ce4c8e682039a4a
dpobera <oberadaveperol@gmail.com>
2022-03-04 16:23:50 +0800

    Update viewsi2.php

--- view/viewsi2.php
+++ view/viewsi2.php
@@ -53,7 +53,7 @@ if (isset($_GET['id']) && is_numeric($_GET['id']) && $_GET['id'] > 0) {
             <p style="position: absolute;left:2.5cm;top:5.9cm;margin:0; font-size:small"> <?php echo $customerAdd?></p>
             <p style="position: absolute;left:2.5cm;top:5.3cm;margin:0;"> </p>
             <p style="position: absolute;left:17cm;top:5.5cm;margin:0;"> <?php echo $drNo?></p>
-            <p style="position: absolute;left:17cm;top:4.8cm;margin:0;"> <?php echo $date?></p>
+            <p style="position: absolute;left:16cm;top:4.8cm;margin:0;"> <?php echo $date?></p>
               <div class="dr_table">
                  <table class="items" style="position: absolute;">
                  <?php
@@ -99,9 +99,9 @@ $anv = $grandTot / 112;
 $anvv = $anv * 100;
 $av = $grandTot - $anvv;
 ?>
- <p style="position: absolute;top:16.2cm;left:18.1cm"><?php echo number_format($anvv, 2)  ?></p>
- <p style="position: absolute;top:17.4cm;left:18.1cm"><?php echo number_format($anv, 2)  ?></p>
- <p style="position: absolute;top:18cm;left:18.1cm"><?php echo number_format($grandTot, 2)  ?></p>
+ <p style="position: absolute;top:15.7cm;left:18.1cm"><?php echo number_format($anvv, 2)  ?></p>
+ <p style="position: absolute;top:17cm;left:18.1cm"><?php echo number_format($anv, 2)  ?></p>
+ <p style="position: absolute;top:17.5cm;left:18.1cm"><?php echo number_format($grandTot, 2)  ?></p>
         </div>
     </div>
 </body>
