773b42832c8d2ad300d4f2826a63ccdc0a3362b6
dpobera <oberadaveperol@gmail.com>
2022-03-21 16:13:42 +0800

    Update index.php

--- pos/index.php
+++ pos/index.php
@@ -327,6 +327,7 @@ if (!$_SESSION['user']) {
               <option value="1">BPI</option>
               <option value="2">BDO</option>
               <option value="3">MBTC</option>
+              <option value="4">CBC</option>
             </select>
             <label for="">Cheque Date:</label>
             <input type="date" class="cheque-date" name="payment_date" disabled />
