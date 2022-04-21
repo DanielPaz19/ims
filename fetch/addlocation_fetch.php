91530c1d08bbb045f2f97d12fb5688bb7df40a77
dpobera <oberadaveperol@gmail.com>
2022-04-21 13:35:02 +0800

    done

---
+++ fetch/addlocation_fetch.php
@@ -0,0 +1,202 @@
+<?php
+
+$connect = new PDO("mysql:host=localhost; dbname=inventorymanagement", "root", "@Dmin898");
+
+/*function get_total_row($connect)
+{
+  $query = "
+  SELECT * FROM tbl_webslesson_post
+  ";
+  $statement = $connect->prepare($query);
+  $statement->execute();
+  return $statement->rowCount();
+}
+
+$total_record = get_total_row($connect);*/
+
+$limit = '10';
+$page = 1;
+if ($_POST['page'] > 1) {
+    $start = (($_POST['page'] - 1) * $limit);
+    $page = $_POST['page'];
+} else {
+    $start = 0;
+}
+
+$query = "
+SELECT * FROM loc_tb ";
+
+if ($_POST['query'] != '') {
+    $query .= '
+  WHERE loc_name LIKE "%' . str_replace(' ', '%', $_POST['query']) . '%" 
+  ';
+}
+$query .= 'ORDER BY loc_id ASC ';
+
+$filter_query = $query . 'LIMIT ' . $start . ', ' . $limit . '';
+
+$statement = $connect->prepare($query);
+$statement->execute();
+$total_data = $statement->rowCount();
+
+$statement = $connect->prepare($filter_query);
+$statement->execute();
+$result = $statement->fetchAll();
+$total_filter_data = $statement->rowCount();
+
+$output = '
+<br>
+<table class="table table-hover table-sm" width="100%" style="cursor:pointer;">
+  <tr style="background-color:#0d6efd;color:white">
+    <th>Location ID</th>
+    <th>Location Name</th>
+
+  </tr>
+';
+if ($total_data > 0) {
+    foreach ($result as $row)
+
+
+        $output .= '
+    <tr>
+      <td>' . str_pad($row["loc_id"], 8, 0, STR_PAD_LEFT) . '</td>
+      <td>' . $row["loc_name"] . '</td>
+     
+    </tr>
+    ';
+} else {
+    $output .= '
+  <tr>
+    <td colspan="10" align="center"><div class="alert alert-danger" role="alert">
+    No Records found !
+   </div></td>
+  </tr>
+  ';
+}
+
+$output .= '
+</table>
+<br />
+<label style="float:right; color:gray;">Total Records - ' . $total_data . '</label>
+<br />
+<div align="center">
+<nav aria-label="Page navigation example">
+  <ul class="pagination">
+';
+
+$total_links = ceil($total_data / $limit);
+$previous_link = '';
+$next_link = '';
+$page_link = '';
+$page_array = [];
+
+//echo $total_links;
+
+if ($total_links > 4) {
+    if ($page < 5) {
+        for ($count = 1; $count <= 5; $count++) {
+            $page_array[] = $count;
+        }
+        $page_array[] = '...';
+        $page_array[] = $total_links;
+    } else {
+        $end_limit = $total_links - 5;
+        if ($page > $end_limit) {
+            $page_array[] = 1;
+            $page_array[] = '...';
+            for ($count = $end_limit; $count <= $total_links; $count++) {
+                $page_array[] = $count;
+            }
+        } else {
+            $page_array[] = 1;
+            $page_array[] = '...';
+            for ($count = $page - 1; $count <= $page + 1; $count++) {
+                $page_array[] = $count;
+            }
+            $page_array[] = '...';
+            $page_array[] = $total_links;
+        }
+    }
+} else {
+    for ($count = 1; $count <= $total_links; $count++) {
+        $page_array[] = $count;
+    }
+}
+
+for ($count = 0; $count < count($page_array); $count++) {
+
+    if ($page == $page_array[$count]) {
+        $page_link .= '
+    <li class="page-item active">
+      <a class="page-link" href="#">' . $page_array[$count] . ' <span class="sr-only"></span></a>
+    </li>
+    ';
+
+        $previous_id = $page_array[$count] - 1;
+        if ($previous_id > 0) {
+            $previous_link = '<li class="page-item"><a class="page-link" href="javascript:void(0)" data-page_number="' . $previous_id . '">&laquo;</a></li>';
+        } else {
+            $previous_link = '
+      <li class="page-item disabled">
+        <a class="page-link" href="#">&laquo;</a>
+      </li>
+      ';
+        }
+        $next_id = $page_array[$count] + 1;
+        if ($next_id >= $total_links) {
+            $next_link = '
+      <li class="page-item disabled">
+        <a class="page-link" href="#">&raquo;</a>
+      </li>
+        ';
+        } else {
+            $next_link = '<li class="page-item"><a class="page-link" href="javascript:void(0)" data-page_number="' . $next_id . '">&raquo;</a></li>';
+        }
+    } else {
+        if ($page_array[$count] == '...') {
+            $page_link .= '
+      <li class="page-item disabled">
+          <a class="page-link" href="#">...</a>
+      </li>
+      ';
+        } else {
+            $page_link .= '
+      <li class="page-item"><a class="page-link" href="javascript:void(0)" data-page_number="' . $page_array[$count] . '">' . $page_array[$count] . '</a></li>
+      ';
+        }
+    }
+}
+
+
+$output .= $previous_link . $page_link . $next_link;
+$output .= '
+  </ul>
+  </nav>
+</div>
+';
+
+echo $output;
+
+?>
+
+<script>
+    function confirmAction() {
+        let confirmAction = confirm("Are you sure you want to delete?");
+        if (confirmAction) {
+            alert("Deleted item successfully!!!");
+        } else {
+
+            alert("Action canceled");
+        }
+    }
+
+    function confirmUpdate() {
+        let confirmUpdate = confirm("Are you sure you want to CONFIRM record?\n \nNote: Double Check Input Records");
+        if (confirmUpdate) {
+            alert("CONFIRM Record successfully!");
+        } else {
+
+            alert("Action Canceled");
+        }
+    }
+</script>
---
+++ utilities/addlocation_con.php
@@ -0,0 +1,25 @@
+<?php
+
+// connect to the database
+include "../php/config.php";
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+// Add item
+if (isset($_POST['addLoc'])) {
+  // receive all input values from the form
+  echo "connect";
+  $loc_name = mysqli_real_escape_string($db, $_POST['loc_name']);
+
+  $query = "INSERT INTO loc_tb (loc_name) 
+          VALUES('$loc_name')";
+
+  if (mysqli_query($db, $query)) {
+    echo "<script>alert('New Record Added')</script>";
+    echo "<script>window.close();</script>";
+  } else {
+    echo '<script type="text/javascript"> alert("Error Uploading Data!"); </script>';  // when error occur
+  }
+}
